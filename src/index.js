e829eaca54bb573a58816d4bce9023bf048e1913
biefchtekz <sydoruk2oo3@gmail.com>
2023-11-16 16:30:58 +0200

    Added main structural components (main layout, home page, error page, router)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import './styles.scss';
+import MainLayout from "./components/shared/main-layout/main-layout";
+import ErrorPage from "./components/pages/error-page/error-page";
+import HomePage from "./components/pages/home-page/home-page";
+
+const router = createBrowserRouter([{
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+        {
+            path: "/",
+            element: <HomePage />
+        }
+    ]
+}])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <RouterProvider router={router}/>
 );
