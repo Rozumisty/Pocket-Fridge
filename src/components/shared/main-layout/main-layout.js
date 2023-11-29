c50ff682cc32b164c12ec04d0f487c7ebed60722
biefchtekz <sydoruk2oo3@gmail.com>
2023-11-29 16:41:06 +0200

    Added navbar to main layout

--- src/components/shared/main-layout/main-layout.js
+++ src/components/shared/main-layout/main-layout.js
@@ -1,7 +1,23 @@
 import {Outlet} from "react-router-dom";
+import styles from "./main-layout.module.scss"
 
 export default function MainLayout() {
     return (<>
-        <Outlet />
+        <div className={styles.navbar}>
+            <div className={`container ${styles.container}`}>
+                <div className={styles.logoContainer}>
+                    <img src="/temp-logo.png" alt="logo" className={styles.logo}/>
+                    <p className={styles.logoTitle}>Pocket Fridge</p>
+                </div>
+                <a href="https://youtu.be/dQw4w9WgXcQ?si=rQvEvdOfaCrMgRPx"
+                   target={"_blank"}
+                   rel="noreferrer"
+                   className={styles.downloadLink}
+                >Download
+                    app</a>
+            </div>
+
+        </div>
+        <Outlet/>
     </>)
 }
