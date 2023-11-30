1aab123152321693ed4e9d26ae7633d82e6bbca2
biefchtekz <sydoruk2oo3@gmail.com>
2023-11-30 21:05:32 +0200

    - Updated Error page

--- src/components/pages/error-page/error-page.js
+++ src/components/pages/error-page/error-page.js
@@ -2,7 +2,7 @@ import styles from "./error-page.module.scss"
 import {Link} from "react-router-dom";
 export default function ErrorPage(){
     return (
-        <div className={styles.errorContainer}>
+        <div className={`${styles.errorContainer} container`}>
             <img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678069-sign-error-512.png" alt="Error" className={styles.errorImg}/>
             <p className={styles.errorMessage}>Where did you find that link?</p>
             <p className={styles.errorMessage}>There's no page here :(</p>
