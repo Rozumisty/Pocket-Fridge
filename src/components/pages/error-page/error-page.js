9030450265d5a69cd7c1e163da75771b2b934be0
biefchtekz <sydoruk2oo3@gmail.com>
2023-11-30 16:22:05 +0200

    - Changed colors - Added error page - Changed logo - Added link meta tags

--- src/components/pages/error-page/error-page.js
+++ src/components/pages/error-page/error-page.js
@@ -1,5 +1,12 @@
+import styles from "./error-page.module.scss"
+import {Link} from "react-router-dom";
 export default function ErrorPage(){
     return (
-        <>ERROR!</>
+        <div className={styles.errorContainer}>
+            <img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678069-sign-error-512.png" alt="Error" className={styles.errorImg}/>
+            <p className={styles.errorMessage}>Where did you find that link?</p>
+            <p className={styles.errorMessage}>There's no page here :(</p>
+            <p className={styles.errorMessage}>But you can still <Link to={'/'} className={styles.returnHome}>Return home</Link></p>
+        </div>
     )
 }
