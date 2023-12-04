a4ebd428950beb8d567861b8becaed7e0e400374
biefchtekz <sydoruk2oo3@gmail.com>
2023-12-04 13:20:53 +0200

    - Added <Our Team> section - Added Firebase Analytics

--- src/components/pages/home-page/home-page.js
+++ src/components/pages/home-page/home-page.js
@@ -1,7 +1,53 @@
 import styles from './home-page.module.scss'
+
+const ourTeam = [
+    {
+        name: 'Yurii Demkiv',
+        role: 'PM, DB Dev',
+        imgUrl: '/Demkiv.jpg'
+    },
+    {
+        name: 'Yurii Hulivatiy',
+        role: 'Team Lead',
+        imgUrl: '/Hulivatiy.jpg'
+    },
+    {
+        name: 'Oleksandr Pidopryhora',
+        role: 'Mobile Dev',
+        imgUrl: '/Sanya.jpg'
+    },
+    {
+        name: 'Ruslan Sydoruk',
+        role: 'Web Developer',
+        imgUrl: '/meXD.jpg'
+    },
+    {
+        name: 'Bohdan Kuzhel',
+        role: 'Backend Dev',
+        imgUrl: '/Kuzhel.jpg'
+    },
+    {
+        name: 'Oleh Fedor',
+        role: 'AI Developer',
+        imgUrl: ''
+    }
+
+]
+
+const teamJSX = (name, role, img) => {
+    return (
+        <div className={styles.teamMember}>
+            <img src={img.length > 0 ? img : 'Ubknown.png'} alt="Portrait"/>
+            <h2>{name}</h2>
+            <p>{role}</p>
+        </div>
+    )
+}
+
 export default function HomePage() {
     return (
         <div className={`container ${styles.container}`}>
+
             <div className={styles.fridgeContainer}>
                 <img src="/banner-2.png" alt="Pocket Fridge" className={styles.fridgeImg}/>
                 <h1>Pocket Fridge</h1>
@@ -10,6 +56,12 @@ export default function HomePage() {
                     A mobile grocery tracking app – an all-in-one solution for organized and effortless kitchen management.
                     Track food expiration dates and discover personalized recipes without any headache!
                 </span>
+            </div>
+            <h1 className={styles.teamTitle}>{'< Rozumisty Team >'}</h1>
+            <div className={styles.teamContainer}>
+                {ourTeam.map(r => {
+                   return teamJSX(r.name, r.role, r.imgUrl)
+                })}
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -5,6 +5,27 @@ import './styles.scss';
 import MainLayout from "./components/shared/main-layout/main-layout";
 import ErrorPage from "./components/pages/error-page/error-page";
 import HomePage from "./components/pages/home-page/home-page";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCy4INwnYRB8ty2VhH49i9dLsXQX2Dvwa4",
+    authDomain: "pocket-fridge-project.firebaseapp.com",
+    projectId: "pocket-fridge-project",
+    storageBucket: "pocket-fridge-project.appspot.com",
+    messagingSenderId: "17742874976",
+    appId: "1:17742874976:web:a30c8b7d9579e1cc103605",
+    measurementId: "G-49593PEE5L"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const router = createBrowserRouter([{
     path: "/",
