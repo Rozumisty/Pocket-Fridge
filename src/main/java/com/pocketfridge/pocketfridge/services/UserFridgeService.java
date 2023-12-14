797ebe51dc3f438fa353d3324588709d14f82069
ednikors <newednikors@gmail.com>
2023-12-14 22:25:20 +0200

    update

--- src/main/java/com/pocketfridge/pocketfridge/services/UserFridgeService.java
+++ src/main/java/com/pocketfridge/pocketfridge/services/UserFridgeService.java
@@ -11,6 +11,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.*;
 import java.sql.Date;
+import java.util.stream.Collectors;
+
+/**
+ * Видалення з БД
+ */
 
 @Service
 public class UserFridgeService implements CommandLineRunner {
@@ -20,37 +25,79 @@ public class UserFridgeService implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
     }
+
 
     //категорія
     public String getUserFridge(String login) throws JsonProcessingException {
+        String sql = "SELECT c.name as category_name, p.name as product_name, uf.quantity, uf.expirationDate " +
+                "FROM UserFridge AS uf " +
+                "LEFT JOIN Products AS p ON uf.productId = p.id " +
+                "LEFT JOIN Categories AS c ON p.categoryId = c.id " +
+                "WHERE uf.userId = ?";
+
+        Map<String, List<Map<String, Object>>> resultMap = jdbcTemp.query(sql,
+                        (rs, rowNum) -> {
+                            String category = rs.getString("category_name");
+                            String product = rs.getString("product_name");
+                            int quantity = rs.getInt("quantity");
+                            String expirationDate = rs.getString("expirationDate");
+
+                            Map<String, Object> result = new HashMap<>();
+                            result.put("category", category);
+                            result.put("product", product);
+                            result.put("quantity", quantity);
+                            result.put("expirationDate", expirationDate);
+
+                            return result;
+                        }, getUserId(login))
+                .stream()
+                .collect(Collectors.groupingBy(
+                        r -> r.get("category").toString(),
+                        HashMap::new,
+                        Collectors.toList())
+                );
+
         ObjectMapper mapper = new ObjectMapper();
-        ArrayList<LinkedHashMap<String, String>> result = new ArrayList<>();
-        String sql = "SELECT productId, quantity, expirationDate FROM UserFridge WHERE userId = ?";
-
-        int userId = Integer.parseInt(getUserId(login));
-
-        List<Map<String, Object>> rows = jdbcTemp.queryForList(sql, userId);
-        for (Map<String, Object> row : rows) {
-            LinkedHashMap<String, String> item = new LinkedHashMap<>();
-            item.put("productId", row.get("productID").toString());
-            item.put("productName", getProductName((int)row.get("productID")));
-            item.put("quantity", row.get("quantity").toString());
-            item.put("expirationDate", row.get("expirationDate").toString());
-            result.add(item);
-        }
-        return mapper.writeValueAsString(result);
+        return mapper.writeValueAsString(resultMap);
     }
 
+    public void addProduct(int userId, String productName, int quantity, Date expirationDate) {
 
-    // додавання кількості продуктів
+        boolean exist = false;
 
-    public void addProduct(int userId, String productName, int quantity, Date expirationDate){
-        String sql = "INSERT INTO UserFridge(userid, productId, quantity, additionDate, expirationDate) " +
-                "VALUES(?, ?, ?, ?, ?)";
-        jdbcTemp.update(sql,
-                userId, getProductId(productName), quantity, java.time.LocalDate.now(), expirationDate);
+        String sql = "SELECT expirationDate FROM UserFridge uf " +
+                "JOIN Products p ON p.id = uf.productId " +
+                "WHERE p.name = ? AND userId = ?";
+
+        List<Date> dates = jdbcTemp.queryForList(sql, Date.class, productName, userId);
+
+        if (!dates.isEmpty()) {
+            for (Date date : dates) {
+                if (date.equals(expirationDate)) {
+                    System.out.println("sttutut");
+                    String sqlUpd = "UPDATE UserFridge SET quantity = quantity + ? " +
+                            "WHERE productId = (SELECT id FROM Products WHERE name = ?)" +
+                            " AND expirationDate = ? AND userId = ?";
+                    jdbcTemp.update(sqlUpd, quantity, productName, date, userId);
+                    exist = true;
+                    break;
+                }
+            }
+        }
+        if (!exist){
+            System.out.println("123");
+            String sqladd = "INSERT INTO UserFridge (userid, productId, quantity, additionDate, expirationDate) " +
+                    "VALUES (?, " +
+                    "(SELECT id FROM Products WHERE name = ?), ?, ?, ?)";
+
+            jdbcTemp.update(sqladd, userId, productName, quantity, java.time.LocalDate.now(), expirationDate);
+        }
+    }
+
+    private Integer getProductId(String productName) {
+        String sql = "SELECT id FROM Products WHERE name = ?";
+        return jdbcTemp.queryForObject(sql, Integer.class, productName);
     }
 
     private String getProductName(int productID) {
@@ -59,10 +106,6 @@ public class UserFridgeService implements CommandLineRunner {
         return jdbcTemp.queryForObject(sql, String.class, productID);
     }
 
-    private Integer getProductId(String productName){
-    String sql = "SELECT id FROM Products WHERE name = ?";
-        return jdbcTemp.queryForObject(sql, Integer.class, productName);
-    }
 
     public String getUserId(String login) {
         String sql = "SELECT id FROM Users WHERE login = ?";
